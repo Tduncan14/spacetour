096d059d00a227ceee05f6bf8b49b75dac3eff3f
tduncan14 <nbatre1@yahoo.com>
2024-11-29 10:37:11 -0600

    keep moving forward

--- src/app/components/Nav/Nav.js
+++ src/app/components/Nav/Nav.js
@@ -2,21 +2,43 @@ import React from 'react'
 import Image from 'next/image'
 import './Nav.css'
 import Link from 'next/link'
+import { Bellefair, Barlow_Condensed } from 'next/font/google';
+
+// Configure the fonts
+const bellefair = Bellefair({
+  subsets: ['latin'], // Specify the subset (default is 'latin')
+  weight: '400', // Bellefair only supports Regular (400)
+});
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ['latin'], // Specify the subset
+  weight: ['400', '700'], // Specify the font weights you need
+});
 
 const Nav = () => {
   return (
     <div className="header">
+
+
+
+
       <div className="leftNav">
-        <Image src="./logo.svg" alt="" height={40} width={40} />
+        <Image src="./logo.svg" alt="" height={55} width={55} />
 
       </div>
 
 
       <div className="rightNav">
-        <Link className="link" href="/"><span>00</span> HOME</Link>
-        <Link className="link" href="/destination"><span>01</span> DESTINATION</Link>
-        <Link className="link" href="/crew"><span>02</span> CREW</Link>
-        <Link className="link" href=""><span>03</span>TECHNOLOGY </Link>
+
+        <div className="line"></div>
+
+        <div className="overlay"></div>
+
+
+        <Link className={` ${barlowCondensed.className}  link special`} href="/"><span>00</span> HOME</Link>
+        <Link className={` ${barlowCondensed.className}  link`} href="/destination"><span>01</span> DESTINATION</Link>
+        <Link className={` ${barlowCondensed.className} link`} href="/crew"><span>02</span> CREW</Link>
+        <Link className={`${barlowCondensed.className} link `} href=""><span>03</span>TECHNOLOGY </Link>
       </div>
 
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,18 @@
 import Image from "next/image";
 import './home.css'
 import Nav from "./components/Nav/Nav";
+import { Bellefair, Barlow_Condensed } from 'next/font/google';
+
+// Configure the fonts
+const bellefair = Bellefair({
+  subsets: ['latin'], // Specify the subset (default is 'latin')
+  weight: '400', // Bellefair only supports Regular (400)
+});
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ['latin'], // Specify the subset
+  weight: ['400', '700'], // Specify the font weights you need
+});
 
 export default function Home() {
   return (
@@ -8,16 +20,43 @@ export default function Home() {
       <Nav />
 
 
-      <div class="leftHome">
+      <div className="content">
+
+        <div class="leftHome">
+
+
+          <div className="wrapperLeft">
+
+            <h5 className={`wrapperfive ${bellefair.className}`}>SO, YOU WANT TO TRAVEL TO</h5>
+
+
+            <h1 className={`spaceR ${barlowCondensed.className}`}> SPACE</h1>
+
+            <div className="para">
+              <p>Let's face; if you want to go to space, you might as well</p>
+              <p>genuinely go outer space and not hover kind of on the</p>
+              <p>edge of it. Well sit back, and relax because we'll give you a</p>
+              <p>truly out of this world experience!</p>
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+        <div class="rightHome">
+
+          <div className="rightWrapper">
+            <button className="rightButton"> <span className={`${bellefair.className} explore`} > Explore</span></button>
+
+          </div>
+
+        </div>
 
 
       </div>
 
-
-
-      <dov class="rightHome">
-
-      </dov>
 
 
     </div>
